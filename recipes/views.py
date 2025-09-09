c7d6025d3a97b34f5862281c076d098a9b42da6f
Raniere Lourenco <raniereslourenco@gmail.com>
2025-09-09 19:15:36 -0300

    Creating apps and configure paths

---
+++ recipes/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+# Create your views here.
+def home(request):
+    return HttpResponse("HOME 1")
+
+def contato(request):
+    return HttpResponse("Contato")
+
+def sobre(request):
+    return HttpResponse("Sobre")
